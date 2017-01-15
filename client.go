30b524c44eed56dca4e4e2a9b4e0c0ef58030722
Tux <write@imaginarycode.com>
2017-01-15 01:28:24 -0500

    Add timeout, tweaks.

--- client.go
+++ client.go
@@ -3,26 +3,29 @@ package votifier
 import (
 	"crypto/rsa"
 	"net"
+	"time"
 )
 
+// Client represents a Votifier client.
 type Client struct {
-	Address   string
-	PublicKey *rsa.PublicKey
+	address   string
+	publicKey *rsa.PublicKey
 }
 
+// NewClient creates a new Votifier client.
 func NewClient(address string, publicKey *rsa.PublicKey) Client {
 	return Client{address, publicKey}
 }
 
-// Sends a vote.
+// SendVote sends a vote through the client.
 func (client Client) SendVote(vote Vote) error {
-	conn, err := net.Dial("tcp", client.Address)
+	conn, err := net.DialTimeout("tcp", client.address, 3*time.Second)
 	if err != nil {
 		return err
 	}
 	defer conn.Close()
 
-	serialized, err := vote.Serialize(client.PublicKey)
+	serialized, err := vote.Serialize(client.publicKey)
 	if err != nil {
 		return err
 	}
